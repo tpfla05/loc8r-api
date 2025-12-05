bf270d57f06457af43dcfd1b089df33dae35e3bf
kimserim <tpfla05@hknu.ac.kr>
2025-12-05 22:15:41 +0900

    fix: environment url update

--- app_public/src/environments/environment.ts
+++ app_public/src/environments/environment.ts
@@ -1,4 +1,10 @@
-export const environment = {
+/* export const environment = {
   apiBaseUrl: 'http://localhost:3000/api/',
   production: false
+};
+*/
+
+export const environment = {
+  apiBaseUrl: 'https://loc8r-api24.onrender.com/api',
+  production: true
 };
